81605413254ed11ead3815000fb77b71643f3f8b
dawin.kweon <dawin.kweon@eroad.com>
2023-09-17 13:10:41 +0900

    only fetch 1 ad per page in development mode to speed up testing cycle

--- pages/api/_client/advertsClient.ts
+++ pages/api/_client/advertsClient.ts
@@ -5,51 +5,22 @@ import path from "path";
 const axios = require("axios");
 
 const findActorAuditionsByPageNumber = async (pageNumber: number) => {
-  const pageUrl = getPageUrl(pageNumber);
-  console.debug(`Scraping page url: ${pageUrl}`);
-  const response = await axios.get(pageUrl);
-  const html = response.data;
-
-  const $ = cheerio.load(html);
-
-  const adverts: Advert[] = [];
-
-  const $boardList = $("div#board-list");
-  $boardList.each((_, board) => {
-    const $mobileNoboldTd = $(board).find('span:contains("배역")');
-
-    $mobileNoboldTd.each((_index, element) => {
-      const age = $(element)[0].next;
-      const title = $(board).find("tbody td:first-child");
-
-      const gender = $(board).find('span:contains("성별")');
-      const genderValue = $(gender)[0].next;
-
-      // @ts-ignore
-      const link = $(board).find("td > a")[0].attribs.href;
-      
-      const linkUrl = new URL(link , Environment.FILM_MAKERS_BASE_URL).href;
-      $(board).find("td > a")[0].attribs.href = linkUrl;
-
-
-      const advert: Advert = {
-        // @ts-ignore
-        title: $(title).html(),
-        // @ts-ignore
-        age: age.data,
-        // @ts-ignore
-        gender: genderValue.data.trim().replace(/\t/g, ""),
-        linkUrl: linkUrl,
-        parentPageIndex: "배우 모집 페이지 #" + pageNumber
-      };
-      adverts.push(advert);
-    });
-  });
-  return adverts;
+  const pageUrl = getActorWantedPageUrl(pageNumber);
+  const category = "배우";
+  return await findAds(category, pageUrl, pageNumber);
 };
 
 const findPerformerWantedByPageNumber = async (pageNumber: number) => {
   const pageUrl = getFindPerformerWantedPageUrl(pageNumber);
+  const category = "모델/출연자";
+  return await findAds(category, pageUrl, pageNumber);
+};
+
+const findAds = async (
+  category: string,
+  pageUrl: string,
+  pageNumber: number
+) => {
   console.debug(`Scraping page url: ${pageUrl}`);
   const response = await axios.get(pageUrl);
   const html = response.data;
@@ -63,6 +34,10 @@ const findPerformerWantedByPageNumber = async (pageNumber: number) => {
     const $mobileNoboldTd = $(board).find('span:contains("배역")');
 
     $mobileNoboldTd.each((_index, element) => {
+      if (Environment.IS_DEVELOPMENT && shouldSkipAds(adverts)) {
+        return;
+      }
+
       const age = $(element)[0].next;
       const title = $(board).find("tbody td:first-child");
 
@@ -71,11 +46,10 @@ const findPerformerWantedByPageNumber = async (pageNumber: number) => {
 
       // @ts-ignore
       const link = $(board).find("td > a")[0].attribs.href;
-      
-      const linkUrl = new URL(link , Environment.FILM_MAKERS_BASE_URL).href;
+
+      const linkUrl = new URL(link, Environment.FILM_MAKERS_BASE_URL).href;
       $(board).find("td > a")[0].attribs.href = linkUrl;
       $(board).find("td > a")[0].attribs.target = "_blank";
-
 
       const advert: Advert = {
         // @ts-ignore
@@ -85,7 +59,7 @@ const findPerformerWantedByPageNumber = async (pageNumber: number) => {
         // @ts-ignore
         gender: genderValue.data.trim().replace(/\t/g, ""),
         linkUrl: linkUrl,
-        parentPageIndex: "모델/출연자 모집 페이지 #" + pageNumber
+        parentPageIndex: `${category} 모집 페이지 #` + pageNumber,
       };
       adverts.push(advert);
     });
@@ -93,18 +67,33 @@ const findPerformerWantedByPageNumber = async (pageNumber: number) => {
   return adverts;
 };
 
-const getPageUrl = (pageNumber: number) : string => {
+const getActorWantedPageUrl = (pageNumber: number): string => {
   return new URL(
-    `/actorsAudition/page/${pageNumber.toString()}?extra_vars8=${encodeURI("여자")}`,
+    `/actorsAudition/page/${pageNumber.toString()}?extra_vars8=${encodeURI(
+      "여자"
+    )}`,
     Environment.FILM_MAKERS_BASE_URL
   ).href;
 };
 
-const getFindPerformerWantedPageUrl = (pageNumber: number) : string => {
+const getFindPerformerWantedPageUrl = (pageNumber: number): string => {
   return new URL(
-    `/performerWanted/page/${pageNumber.toString()}?extra_vars8=${encodeURI("여자")}`,
+    `/performerWanted/page/${pageNumber.toString()}?extra_vars8=${encodeURI(
+      "여자"
+    )}`,
     Environment.FILM_MAKERS_BASE_URL
   ).href;
 };
 
-export const advertsClient = { findActorAuditionsByPageNumber, findPerformerWantedByPageNumber };
+export const advertsClient = {
+  findActorAuditionsByPageNumber,
+  findPerformerWantedByPageNumber,
+};
+function shouldSkipAds(adverts: Advert[]) {
+  const hasEnoughAds = adverts.length > 1;
+  if (hasEnoughAds) {
+    console.log("Only returning 1 result due to development mode");
+    // Only return 1 when in development
+    return true;
+  }
+}
--- pages/api/_env.ts
+++ pages/api/_env.ts
@@ -1,4 +1,8 @@
 export const Environment = {
+    IS_DEVELOPMENT: process.env.NODE_ENV === "development",
+
     USE_MOCK_ADVERT_DATA: process.env.NEXT_PUBLIC_USE_MOCK_ADVERT_DATA === "true",
-    FILM_MAKERS_BASE_URL: process.env.FILM_MAKERS_BASE_URL ?? "https://www.filmmakers.co.kr/"
+    FILM_MAKERS_BASE_URL: process.env.FILM_MAKERS_BASE_URL ?? "https://www.filmmakers.co.kr/",
 }
+
+console.log("IS_DEVELOPMENT", Environment.IS_DEVELOPMENT);
